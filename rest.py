74a891e263ffdd67ed17f0e29a226e46b7a0e550
saranyaks <saranyaks515@gmail.com>
2016-06-09 11:36:29 +0530

    Adding PEP-8 standards.

--- rest.py
+++ rest.py
@@ -1,47 +1,46 @@
-import web
-import boto3
 import datetime
 import logging
 
-logging.basicConfig(filename='pythonlog.log', filemode='w',format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
-logging.info('------STARTING APPLICATION------')
+import web
+import boto3
+
+logging.basicConfig(
+     filename='pythonlog.log', filemode='w',
+     format='%(asctime)s %(levelname)s: %(message)s',
+     level=logging.INFO)
 
 urls = (
-    '/clicks', 'get_clicks'
-)
-
+    '/clicks', 'get_clicks')
 app = web.application(urls, globals())
 
-class get_clicks:        
+
+class get_clicks:
     def GET(self):
-	      dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('prod1.ad-processor.banner-clicks-aggregation.date-shift.DainikBhaskar')
+        """
+        Return updated bannerclick aggregation table data for today.
+        """
+        logging.info('------STARTING APPLICATION------')
+        dynamodb = boto3.resource('dynamodb')
+        table_name = "prod1.ad-processor.banner-clicks-aggregation.date-shift.DainikBhaskar"
+        table = dynamodb.Table(table_name)
         date = datetime.date.today().strftime("%Y-%m-%d")
-        logging.info('Querying table with qualifier : testing://c1sdk/p1sdk and date : ' + date)
-        result  = table.get_item(
+        logging.info('Querying table with qualifier: testing://c1sdk/p1sdk and date: ' + date)
+        result = table.get_item(
                        Key={
-
                         'qualifier': 'testing://c1sdk/p1sdk',
-                        'targetDay': date
-                      }
-                  )
-
-        
-        count = result['Item']['clickCount']
+                        'targetDay': date})  # Querying dynamodb table.
+        count = result['Item']['clickCount']  # Retrieving current click count.
         val = str(count)
         logging.info('Current click count' + val)
-        count = count + 1
-        logging.info('Updating table ....')
+        count += 1
+        logging.info('Updating table...')
         table.put_item(
             Item={
               'clickCount': count,
               'qualifier': 'testing://c1sdk/p1sdk',
-              'targetDay': date
-            }
-        )
-        logging.info('Table updated..')
-        return '{ clickCount : ' + count + '}'
-
+              'targetDay': date})  # Updating dynamodb table.
+        logging.info('Table updated...')
+        return '{clickCount: '+ count + '}'
 
 
 if __name__ == "__main__":
